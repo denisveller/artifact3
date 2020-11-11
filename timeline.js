8f96c9ab02514f8fcf6df5a7d19c705b9fa594d3
Denis Veller <denisveller@macbook-pro.lan>
2020-11-11 13:37:01 -0500

    first commit

---
+++ timeline.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('timeline');
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.width;
+var lineWeight = 2;
+var mainLineWeight = 10;
