152be24529958d4517cfa86e2be86667ba6bb3bb
Denis Veller <denis.veller@gmail.com>
2020-11-11 23:37:50 -0500

    improved mobile expeience

--- timeline.js
+++ timeline.js
@@ -1,1 +1,14 @@
 $('body').scrollspy({ target: '#navbar-example3' })
+function toggleTimelineVisibility() {
+    var x = document.getElementById("timeline");
+    var y = document.getElementById("return");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+        x.style.width = "100%";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        x.style.width = "25%";
+    }
+}
