cd8f7bdd262d693164818d08f1bf6791c81a9f83
Denis Veller <denis.veller@gmail.com>
2020-11-11 22:17:19 -0500

    poem 1

--- timeline.js
+++ timeline.js
@@ -1,11 +1,1 @@
-/*
-var p = document.getElementById("fillUp");
-var h = document.body.scrollHeight;
-const hc = h;
-while (h==hc){
-    temp = p.innerHTML;
-    p.innerHTML = temp + 'g<br>'
-    var h = document.body.scrollHeight;
-}
-*/
 $('body').scrollspy({ target: '#navbar-example3' })
