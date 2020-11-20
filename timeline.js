20058cd02ccbaf25a03aeb64b841b84dcbebe8d0
Denis Veller <denis.veller@gmail.com>
2020-11-19 23:46:09 -0500

    1

--- timeline.js
+++ timeline.js
@@ -13,3 +13,18 @@ function toggleTimelineVisibility() {
         x.style.width = "25%";
     }
 }
+var r1 = document.getElementById('r1');
+var e1 = document.getElementById('e1');
+var r2 = document.getElementById('r2');
+var e2 = document.getElementById('e2');
+var r3 = document.getElementById('r3');
+var e3 = document.getElementById('e3');
+var r4 = document.getElementById('r4');
+var e4 = document.getElementById('e4');
+
+function playAudio(x){
+    x.play();
+}
+function playAudio(x){
+    x.pause();
+}
